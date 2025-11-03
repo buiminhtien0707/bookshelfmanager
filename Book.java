a80c277441170b1cd87af019028bb940a9c420a3
Bùi Minh Tiến <118090719+buiminhtien0707@users.noreply.github.com>
2025-11-03 11:48:37 +0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,6 +4,50 @@
  *
  */
 public class Book {
-    
+    private String id;
+    private String title;
+    private String author;
+    private String notes;
 
+    // Constructor
+    public Book(String id, String title, String author, String notes) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.notes = notes;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
